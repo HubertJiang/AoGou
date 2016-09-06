5c3f80bf5bce1b231e149727213f478d33856f2a
HubertJiang <xkjiang@hotmail.com>
2016-09-06 10:12:19 +0800

    list

---
+++ app/src/main/java/com/kuiyuan/aogou/adapter/MainAdapter.java
@@ -0,0 +1,40 @@
+package com.kuiyuan.aogou.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+import com.kuiyuan.aogou.R;
+import com.kuiyuan.aogou.ViewHolder.MainViewHolder;
+import com.kuiyuan.aogou.entity.Goods;
+
+import java.util.List;
+
+/**
+ * Created by jiangkuiyuan on 16/9/4.
+ */
+public class MainAdapter extends BaseAdapter<Goods> {
+    public MainAdapter(Context context, RecyclerView recyclerView, List<Goods> data) {
+        super(context, recyclerView, data);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.item_main, parent, false);
+        return new MainViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof MainViewHolder) {
+            Goods goods = getItem(position);
+            ((MainViewHolder) holder).name.setText(goods.name);
+            ((MainViewHolder) holder).content.setText(goods.content);
+            if(goods.image!=null)
+            Glide.with(getContext()).load(goods.image.getUrl()).into(((MainViewHolder)holder).imageView);
+        }
+    }
+}
---
+++ app/src/main/java/com/kuiyuan/aogou/entity/Goods.java
@@ -0,0 +1,12 @@
+package com.kuiyuan.aogou.entity;
+
+import cn.bmob.v3.datatype.BmobFile;
+
+/**
+ * Created by jiangkuiyuan on 16/9/4.
+ */
+public class Goods {
+    public String name;
+    public String content;
+    public BmobFile image;
+}
