cfaf275e8637e45531c30d3285a9e406df9a1227
HubertJiang <xkjiang@hotmail.com>
2016-09-07 16:25:00 +0800

    initialize

---
+++ app/src/main/java/com/kuiyuan/aogou/activity/GoodsDetailActivity.java
@@ -0,0 +1,33 @@
+package com.kuiyuan.aogou.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.kuiyuan.aogou.R;
+import com.kuiyuan.aogou.entity.Goods;
+
+public class GoodsDetailActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private Goods goods;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goods_detail);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/kuiyuan/aogou/entity/Goods.java
+++ app/src/main/java/com/kuiyuan/aogou/entity/Goods.java
@@ -1,11 +1,12 @@
 package com.kuiyuan.aogou.entity;
 
+import cn.bmob.v3.BmobObject;
 import cn.bmob.v3.datatype.BmobFile;
 
 /**
  * Created by jiangkuiyuan on 16/9/4.
  */
-public class Goods {
+public class Goods extends BmobObject {
     public String name;
     public String content;
     public BmobFile image;
