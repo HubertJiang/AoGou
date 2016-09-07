328292fd0c85cadb5ea6dcff675a5839993307ce
HubertJiang <jiangxiankui@liweihui.com>
2016-09-07 16:25:00 +0800

    initialize

--- app/src/main/java/com/kuiyuan/aogou/adapter/MainAdapter.java
+++ app/src/main/java/com/kuiyuan/aogou/adapter/MainAdapter.java
@@ -1,6 +1,7 @@
 package com.kuiyuan.aogou.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import com.bumptech.glide.Glide;
 import com.kuiyuan.aogou.R;
 import com.kuiyuan.aogou.ViewHolder.MainViewHolder;
+import com.kuiyuan.aogou.activity.GoodsDetailActivity;
 import com.kuiyuan.aogou.entity.Goods;
 
 import java.util.List;
@@ -35,6 +37,14 @@ public class MainAdapter extends BaseAdapter<Goods> {
             ((MainViewHolder) holder).content.setText(goods.content);
             if(goods.image!=null)
             Glide.with(getContext()).load(goods.image.getUrl()).into(((MainViewHolder)holder).imageView);
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent=new Intent(getContext(), GoodsDetailActivity.class);
+                    getContext().startActivity(intent);
+                }
+            });
         }
     }
 }
