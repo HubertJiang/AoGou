6e5e80f44cc498a40602aa237759ae1e9c71b195
HubertJiang <xkjiang@hotmail.com>
2016-09-08 10:51:28 +0800

    initialize

--- app/src/main/java/com/kuiyuan/aogou/fragment/MainFragment.java
+++ app/src/main/java/com/kuiyuan/aogou/fragment/MainFragment.java
@@ -3,6 +3,7 @@ package com.kuiyuan.aogou.fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -26,34 +27,77 @@ import cn.bmob.v3.listener.FindListener;
 public class MainFragment extends Fragment {
     private RecyclerView recyclerView;
     private MainAdapter adapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private int lastVisibleItem, page;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_layout);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         adapter = new MainAdapter(getActivity(), recyclerView, null);
         recyclerView.setAdapter(adapter);
 
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimaryDark);
+//        swipeRefreshLayout.setProgressBackgroundColorSchemeResource(R.color.colorPrimaryDark);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                page = 0;
+                get();
+            }
+        });
+
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView,
+                                             int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE
+                        && lastVisibleItem + 1 == adapter.getItemCount()) {
+                    swipeRefreshLayout.setRefreshing(true);
+                    get();
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                lastVisibleItem = ((LinearLayoutManager) recyclerView.getLayoutManager()).findLastVisibleItemPosition();
+            }
+        });
+
+        return view;
+    }
+
+    private void get() {
         BmobQuery<Goods> query = new BmobQuery<>();
-        query.setCachePolicy(BmobQuery.CachePolicy.CACHE_THEN_NETWORK);
+//        query.setCachePolicy(BmobQuery.CachePolicy.CACHE_THEN_NETWORK);
         query.addQueryKeys("name,content,image");
 //查询playerName叫“比目”的数据
 //返回50条数据，如果不加上这条语句，默认返回10条数据
-        query.setLimit(50);
+        query.setLimit(8);
+        query.setSkip(8 * page);
 //执行查询方法
         query.findObjects(new FindListener<Goods>() {
             @Override
             public void done(List<Goods> object, BmobException e) {
+                swipeRefreshLayout.setRefreshing(false);
                 if (e == null) {
-                    adapter.setData(object);
-
+                    page++;
+                    if (page == 0) {
+                        adapter.setData(object);
+                    } else {
+                        adapter.addAll(object);
+                    }
                 } else {
                     Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
-        return view;
     }
 }
