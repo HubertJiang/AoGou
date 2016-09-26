fc3fd0282f0c3fb3ae95fb33695311d12e0329c5
HubertJiang <jiangxiankui@liweihui.com>
2016-09-26 16:14:40 +0800

    修改列表样式

--- app/src/main/java/com/kuiyuan/aogou/activity/GoodsDetailActivity.java
+++ app/src/main/java/com/kuiyuan/aogou/activity/GoodsDetailActivity.java
@@ -6,22 +6,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.kuiyuan.aogou.R;
 import com.kuiyuan.aogou.entity.Goods;
+import com.kuiyuan.aogou.entity.User;
 
 import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.datatype.BmobRelation;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.QueryListener;
+import cn.bmob.v3.listener.UpdateListener;
 
-public class GoodsDetailActivity extends AppCompatActivity {
+public class GoodsDetailActivity extends AppCompatActivity implements View.OnClickListener {
     private String id;
-    private TextView content;
+    private TextView content,likesTextView;
     private ImageView imageView;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private Goods goods;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +39,12 @@ public class GoodsDetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         content= (TextView) findViewById(R.id.content);
+        likesTextView= (TextView) findViewById(R.id.likes_text_view);
         imageView= (ImageView) findViewById(R.id.image);
         swipeRefreshLayout= (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
         swipeRefreshLayout.setProgressViewOffset(true, 0, 500);
+
+        likesTextView.setOnClickListener(this);
 
         swipeRefreshLayout.setRefreshing(true);
         BmobQuery<Goods> query = new BmobQuery<>();
@@ -43,6 +52,7 @@ public class GoodsDetailActivity extends AppCompatActivity {
 
             @Override
             public void done(Goods object, BmobException e) {
+                goods=object;
                 swipeRefreshLayout.setRefreshing(false);
                 swipeRefreshLayout.setEnabled(false);
                 if(e==null){
@@ -65,5 +75,27 @@ public class GoodsDetailActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.likes_text_view:
+                BmobRelation relation=new BmobRelation();
+                relation.add(goods);
+                User newUser = new User();
+                newUser.setLikes(relation);
+                newUser.update(BmobUser.getCurrentUser().getObjectId(),new UpdateListener() {
+                    @Override
+                    public void done(BmobException e) {
+                        if(e==null){
+                            Log.i("bmob","多对多关联添加成功");
+                        }else{
+                            Log.i("bmob","失败："+e.getMessage());
+                        }
+                    }
+                });
+                break;
+        }
     }
 }
--- app/src/main/java/com/kuiyuan/aogou/fragment/SettingFragment.java
+++ app/src/main/java/com/kuiyuan/aogou/fragment/SettingFragment.java
@@ -1,12 +1,14 @@
 package com.kuiyuan.aogou.fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.kuiyuan.aogou.R;
+import com.kuiyuan.aogou.activity.LikesActivity;
 
 import cn.bmob.v3.BmobUser;
 
@@ -18,7 +20,7 @@ import cn.bmob.v3.BmobUser;
  * Use the {@link SettingFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingFragment extends Fragment {
+public class SettingFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -64,17 +66,23 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_setting, container, false);
-        view.findViewById(R.id.exit).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BmobUser.logOut();
-                getActivity().finish();
-            }
-        });
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        view.findViewById(R.id.likes).setOnClickListener(this);
+        view.findViewById(R.id.exit).setOnClickListener(this);
         return view;
     }
 
 
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.likes:
+                startActivity(new Intent(getActivity(), LikesActivity.class));
+                break;
+            case R.id.exit:
+                BmobUser.logOut();
+                getActivity().finish();
+                break;
+        }
+    }
 }
