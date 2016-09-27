35f70a45625debc97884a74c7ea4f177b95a1c9b
HubertJiang <xkjiang@hotmail.com>
2016-09-27 16:45:32 +0800

    增加商品分类

---
+++ app/src/main/java/com/kuiyuan/aogou/entity/Classify.java
@@ -0,0 +1,11 @@
+package com.kuiyuan.aogou.entity;
+
+import cn.bmob.v3.BmobObject;
+
+/**
+ * Created by liweihui on 2016/9/27.
+ */
+
+public class Classify extends BmobObject {
+    public String name;
+}
