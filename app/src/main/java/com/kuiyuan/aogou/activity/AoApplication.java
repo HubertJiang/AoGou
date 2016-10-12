a53dd65998ef637baf72c42f031612465d9612fc
HubertJiang <xkjiang@hotmail.com>
2016-10-12 14:57:40 +0800

    商品详情

--- app/src/main/java/com/kuiyuan/aogou/activity/AoApplication.java
+++ app/src/main/java/com/kuiyuan/aogou/activity/AoApplication.java
@@ -1,6 +1,8 @@
 package com.kuiyuan.aogou.activity;
 
+import android.app.ActivityManager;
 import android.app.Application;
+import android.content.Context;
 import android.widget.Toast;
 
 import cn.bmob.v3.Bmob;
@@ -18,7 +20,25 @@ public class AoApplication extends Application {
         super.onCreate();
         instance = this;
         Bmob.initialize(this, "3e6164ca438e7534e0174f7c6ae78247");
-        RongIM.init(this);
+
+        /**
+         *
+         * OnCreate 会被多个进程重入，这段保护代码，确保只有您需要使用 RongIM 的进程和 Push 进程执行了 init。
+         * io.rong.push 为融云 push 进程名称，不可修改。
+         */
+        if (getApplicationInfo().packageName.equals(getCurProcessName(getApplicationContext())) ||
+                "io.rong.push".equals(getCurProcessName(getApplicationContext()))) {
+
+            /**
+             * IMKit SDK调用第一步 初始化
+             */
+            RongIM.init(this);
+
+            if (getApplicationInfo().packageName.equals(getCurProcessName(getApplicationContext()))) {
+
+//                DemoContext.init(this);
+            }
+        }
     }
 
     public static void showToast(String string) {
@@ -41,5 +61,29 @@ public class AoApplication extends Application {
 
     public static Application getInstance() {
         return instance;
+    }
+
+    /**
+     * 获得当前进程的名字
+     *
+     * @param context
+     * @return
+     */
+    public static String getCurProcessName(Context context) {
+
+        int pid = android.os.Process.myPid();
+
+        ActivityManager activityManager = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+
+        for (ActivityManager.RunningAppProcessInfo appProcess : activityManager
+                .getRunningAppProcesses()) {
+
+            if (appProcess.pid == pid) {
+
+                return appProcess.processName;
+            }
+        }
+        return null;
     }
 }
