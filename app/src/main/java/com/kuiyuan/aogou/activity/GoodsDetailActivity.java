bb9687f97533f07e98a1c5d0d1af3fde4651cd3f
HubertJiang <xkjiang@hotmail.com>
2016-11-15 16:33:34 +0800

    bottom navigation

--- app/src/main/java/com/kuiyuan/aogou/activity/GoodsDetailActivity.java
+++ app/src/main/java/com/kuiyuan/aogou/activity/GoodsDetailActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -29,7 +30,7 @@ import io.rong.imkit.RongIM;
 
 public class GoodsDetailActivity extends AppCompatActivity implements View.OnClickListener {
     private String id;
-    private TextView content,likesTextView,serviceTextView;
+    private TextView content, likesTextView, serviceTextView;
     private ViewPager viewPager;
     private CirclePageIndicator circlePageIndicator;
     private SwipeRefreshLayout swipeRefreshLayout;
@@ -40,17 +41,17 @@ public class GoodsDetailActivity extends AppCompatActivity implements View.OnCli
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_detail);
-        id=getIntent().getStringExtra("id");
-        Toolbar  toolbar = (Toolbar) findViewById(R.id.toolbar);
+        id = getIntent().getStringExtra("id");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        content= (TextView) findViewById(R.id.content);
-        likesTextView= (TextView) findViewById(R.id.likes_text_view);
-        serviceTextView= (TextView) findViewById(R.id.service_text_view);
-        viewPager= (ViewPager) findViewById(R.id.viewPager);
-        circlePageIndicator= (CirclePageIndicator) findViewById(R.id.indicator);
-        swipeRefreshLayout= (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        content = (TextView) findViewById(R.id.content);
+        likesTextView = (TextView) findViewById(R.id.likes_text_view);
+        serviceTextView = (TextView) findViewById(R.id.service_text_view);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        circlePageIndicator = (CirclePageIndicator) findViewById(R.id.indicator);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
         swipeRefreshLayout.setProgressViewOffset(true, 0, 500);
         swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimaryDark);
 
@@ -58,34 +59,40 @@ public class GoodsDetailActivity extends AppCompatActivity implements View.OnCli
         likesTextView.setOnClickListener(this);
         serviceTextView.setOnClickListener(this);
         swipeRefreshLayout.setRefreshing(true);
+swipeRefreshLayout.setOnTouchListener(new View.OnTouchListener() {
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return true;
+    }
+});
 
         BmobQuery<Goods> query = new BmobQuery<>();
         query.getObject(id, new QueryListener<Goods>() {
 
             @Override
             public void done(Goods object, BmobException e) {
-                goods=object;
-                swipeRefreshLayout.setRefreshing(false);
-                swipeRefreshLayout.setEnabled(false);
-                if(e==null){
+                goods = object;
+//                swipeRefreshLayout.setRefreshing(false);
+//                swipeRefreshLayout.setEnabled(false);
+                if (e == null) {
                     getSupportActionBar().setTitle(object.name);
                     content.setText(object.content);
-                    ArrayList<BmobFile> images=new ArrayList<>();
+                    ArrayList<BmobFile> images = new ArrayList<>();
                     images.add(object.image);
-                    if(object.image1!=null){
+                    if (object.image1 != null) {
                         images.add(object.image1);
                     }
-                    if(object.image2!=null){
+                    if (object.image2 != null) {
                         images.add(object.image2);
                     }
-                    if(object.image3!=null){
+                    if (object.image3 != null) {
                         images.add(object.image3);
                     }
                     adapter.setImages(images);
                     viewPager.setAdapter(adapter);
                     circlePageIndicator.setViewPager(viewPager);
-                }else{
-                    Log.i("bmob","失败："+e.getMessage()+","+e.getErrorCode());
+                } else {
+                    Log.i("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
                 }
             }
 
@@ -104,19 +111,19 @@ public class GoodsDetailActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.likes_text_view:
-                BmobRelation relation=new BmobRelation();
+                BmobRelation relation = new BmobRelation();
                 relation.add(goods);
                 User newUser = new User();
                 newUser.setLikes(relation);
-                newUser.update(BmobUser.getCurrentUser().getObjectId(),new UpdateListener() {
+                newUser.update(BmobUser.getCurrentUser().getObjectId(), new UpdateListener() {
                     @Override
                     public void done(BmobException e) {
-                        if(e==null){
-                            Log.i("bmob","多对多关联添加成功");
-                        }else{
-                            Log.i("bmob","失败："+e.getMessage());
+                        if (e == null) {
+                            Log.i("bmob", "多对多关联添加成功");
+                        } else {
+                            Log.i("bmob", "失败：" + e.getMessage());
                         }
                     }
                 });
