a8f2733f4b539def5c3bfb48ac14a47e07002399
HubertJiang <xkjiang@hotmail.com>
2017-03-29 18:00:20 +0800

    load more

--- app/src/main/java/com/kui/gou/activity/WelcomeActivity.java
+++ app/src/main/java/com/kui/gou/activity/WelcomeActivity.java
@@ -1,7 +1,12 @@
 package com.kui.gou.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import com.kui.gou.R;
@@ -13,15 +18,33 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //申请WRITE_EXTERNAL_STORAGE权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    1);
+        }else {
+
+        }
+
 
 //        if (BmobUser.getCurrentUser() == null) {
 //            startActivity(new Intent(this, SignInActivity.class));
 //        } else {
-            startActivity(new Intent(this, MainActivity.class));
+
 //        }
 
-        finish();
     }
 
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                startActivity(new Intent(this, MainActivity.class));
+            }else{
+                AoApplication.showToast("拒绝了打电话权限");
+            }
+        }
+    }
 }
